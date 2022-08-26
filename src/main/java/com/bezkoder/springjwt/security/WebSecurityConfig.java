4fd573df1f016247bd2d289753b68822bbae1094
Adyl Toksumbaev <gucci0701a@gmail.com>
2022-08-26 19:06:05 +0600

    Initial commit

--- src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -54,10 +54,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable()
-      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+    http
+            .cors()
+            .and()
+            .csrf().disable()
+      .exceptionHandling()
+            .authenticationEntryPoint(unauthorizedHandler).and()
+      .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+      .authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
       .antMatchers("/api/test/**").permitAll()
       .anyRequest().authenticated();
 
